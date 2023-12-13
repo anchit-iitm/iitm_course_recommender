3cbf5cddd5c8699d0962f15ab9692a5bf8eb8bcd
Anhat Singh <anhatsingh2001@gmail.com>
2023-12-14 02:54:50 +0530

    minor name changes

--- se-frontend-vue/src/router/index.js
+++ se-frontend-vue/src/router/index.js
@@ -37,7 +37,7 @@ const routes = [
         {
           name: 'AdminsList',
           path: '/admin/all',
-          component: () => import('@/views/CourseTeamView.vue'),
+          component: () => import('@/views/AdminAllView.vue'),
       },
 
     ]
